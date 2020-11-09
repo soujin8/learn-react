7b687bc90047f20520bae568bb40847613c4d996
m-88888888 <hachi8810@gmail.som>
2020-11-10 00:50:16 +0900

    useState復習

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,60 @@
 import React from "react";
+import { Dialog, DialogTitle, Typography, Button } from "@material-ui/core";
 
-function App() {
+type AppProps = {
+  hoge: String;
+};
+
+const App: React.FC<AppProps> = (props) => {
+  const [open, setOpen] = React.useState(false);
+  const [test, setTest] = React.useState("hoge");
+
   return (
-    <div className="App">
-      <h1>Hello, React!</h1>
-    </div>
+    <>
+      <Chil
+        onClickButton={(test: string): void => {
+          setOpen(true);
+          setTest(test);
+        }}
+      />
+      <Dialog
+        open={open}
+        onClose={(): void => {
+          setOpen(false);
+        }}
+      >
+        <DialogTitle>{test}</DialogTitle>
+      </Dialog>
+    </>
   );
-}
+};
+
+type ChilProps = {
+  onClickButton?: (test: string) => void | Promise<void>;
+};
+
+const Chil: React.FC<ChilProps> = (props) => {
+  const { onClickButton = () => {} } = props;
+
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <Button
+        color="primary"
+        onClick={(): void => {
+          onClickButton("Updated Hoge");
+        }}
+      >
+        開く
+      </Button>
+    </>
+  );
+};
+
+// class App extends React.Component<AppProps, {}> {
+//   render() {
+//     return <h1>Hello, {this.props.hoge}</h1>;
+//   }
+// }
 
 export default App;
