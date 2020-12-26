6102c7724a3739c7771628c83473630b13d949c0
m-88888888 <mochi.h8888@gmail.com>
2020-12-26 16:20:34 +0900

    useCallbackはいつかやる。useRefなんとなく理解

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ContextApp from "./components/ContextSample/ContextApp";
 import Reducer from "./components/ReducerSample/Reducer";
 import Counter from "./components/StateSample/Counter";
+import Callback from "./components/CallbackSample/Callback";
+import Ref from "./components/RefSample/Ref";
 
 const App: React.FC = () => {
   return (
@@ -9,6 +11,8 @@ const App: React.FC = () => {
       {/* <ContextApp /> */}
       {/* <Counter /> */}
       {/* <Reducer /> */}
+      {/* <Callback /> */}
+      <Ref />
     </>
   );
 };
---
+++ src/components/CallbackSample/Callback.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Callback: React.FC = () => {
+  return (
+    <>
+      {/* TODO いつかやる */}
+      <h1>hello</h1>
+      <h2>world</h2>
+    </>
+  );
+};
+
+export default Callback;
