b187ab6c50352e8e414609805ae477b6954b13b7
m-88888888 <mochi.h8888@gmail.com>
2020-12-27 20:00:23 +0900

    input formを追加

--- src/components/DataFetch/DataFetch.tsx
+++ src/components/DataFetch/DataFetch.tsx
@@ -3,21 +3,35 @@ import axios from "axios";
 
 const DataFetch: React.FC = () => {
   const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState("redux");
+  const [url, setUrl] = useState(
+    `http://hn.algolia.com/api/v1/search?query=redux`
+  );
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(
-        "http://hn.algolia.com/api/v1/search?query=redux"
-      );
+      const result = await axios(url);
       setData(result.data);
-      console.log(result.data);
     };
 
     fetchData();
-  }, []);
+  }, [url]);
 
   return (
     <>
+      <input
+        type="text"
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+      />
+      <button
+        type="button"
+        onClick={() =>
+          setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
+        }
+      >
+        Search
+      </button>
       <ul>
         {data.hits.map((item: any) => (
           <li key={item.objectID}>
