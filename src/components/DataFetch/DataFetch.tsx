a1b7219e3e622da3de1cb91fbb685b1dbcedc17a
m-88888888 <mochi.h8888@gmail.com>
2020-12-30 17:46:03 +0900

    useReducerで置き換え

--- src/components/DataFetch/DataFetch.tsx
+++ src/components/DataFetch/DataFetch.tsx
@@ -1,25 +1,56 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import axios from "axios";
 
+const dataFetchReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      }
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload
+      }
+    case 'FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      }
+    default:
+      throw new Error()
+  }
+}
+
 const useDataApi = (initialUrl: any, initialData: any) => {
-  const [data, setData] = useState(initialData);
+  // const [data, setData] = useState(initialData);
   const [url, setUrl] = useState(
     initialUrl
   )
   const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false)
-  const doFetch = (url: string) => setUrl(url)
+  // const [isError, setIsError] = useState(false)
+  // const doFetch = (url: string) => setUrl(url)
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData
+  })
 
   useEffect(() => {
     const fetchData = async () => {
-      setIsError(false)
-      setIsLoading(true)
+      dispatch({type: 'FETCH_INIT'})
 
       try {
         const result = await axios(url);
-        setData(result.data);
+        dispatch({type: 'FETCH_SUCCESS', payload: result.data})
+  //      setData(result.data);
       } catch(error) {
-        setIsError(true)
+        dispatch({type: 'FETCH_FAILURE'})
       }
 
       setIsLoading(false)
@@ -28,21 +59,27 @@ const useDataApi = (initialUrl: any, initialData: any) => {
     fetchData();
   }, [url]);
 
-  return { data, isLoading, isError, doFetch}
+  //return { data, isLoading, isError, doFetch}
+  return [state, setUrl]
 }
 
 const DataFetch: React.FC = () => {
   const [query, setQuery] = useState("redux");
-  const { data, isLoading, isError, doFetch} = useDataApi(
+//  const { data, isLoading, isError, doFetch} = useDataApi(
+//   'http://hn.algolia.com/api/v1/search?query=redux',
+//   { hits: [] }
+//  );
+  const [state, setUrl] = useDataApi(
    'http://hn.algolia.com/api/v1/search?query=redux',
    { hits: [] }
-  );
+  )
 
   return (
     <>
       <form
         onSubmit={event => {
-          doFetch(`http://hn.algolia.com/api/v1/search?query=${query}`)
+          //doFetch(`http://hn.algolia.com/api/v1/search?query=${query}`)
+          setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`)
           event.preventDefault()
         }}
       >
@@ -56,13 +93,13 @@ const DataFetch: React.FC = () => {
       </button>
       </form>
 
-      {isError && <div>Something went wrong ...</div>}
+      {state.isError && <div>Something went wrong ...</div>}
 
-      {isLoading ? (
+      {state.isLoading ? (
         <div>Loading...</div>
       ) : (
       <ul>
-        {data.hits.map((item: any) => (
+        {state.data.hits.map((item: any) => (
           <li key={item.objectID}>
             <a href={item.url}>{item.title}</a>
           </li>
