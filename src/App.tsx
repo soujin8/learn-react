33443f9668761879ddb8b2ae9e658fb79e0a10d4
m-88888888 <mochi.h8888@gmail.com>
2021-01-06 00:24:10 +0900

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import ContextApp from "./components/ContextSample/ContextApp";
-import Reducer from "./components/ReducerSample/Reducer";
-import Counter from "./components/StateSample/Counter";
-import Callback from "./components/CallbackSample/Callback";
+// import ContextApp from "./components/ContextSample/ContextApp";
+// import Reducer from "./components/ReducerSample/Reducer";
+// import Counter from "./components/StateSample/Counter";
+// import Callback from "./components/CallbackSample/Callback";
 // import Ref from "./components/RefSample/Ref";
-import DataFetch from "./components/DataFetch/DataFetch";
+// import DataFetch from "./components/DataFetch/DataFetch";
 
 const App: React.FC = () => {
   return (
@@ -14,7 +14,7 @@ const App: React.FC = () => {
       {/* <Reducer /> */}
       {/* <Callback /> */}
       {/* <Ref /> */}
-      <DataFetch />
+      {/*<DataFetch /> */}
     </>
   );
 };
